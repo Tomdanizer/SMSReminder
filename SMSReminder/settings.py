b4955fb40f35bbaf0213ef1c73731bfeea1452d3
Dan T <tomdanizer@gmail.com>
2014-05-09 14:31:10 +0000

    heroku push

--- SMSReminder/settings.py
+++ SMSReminder/settings.py
@@ -80,12 +80,18 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'smsreminder',
+#        'USER': 'root',
+#            'HOST': '0.0.0.0',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smsreminder',
-        'USER': 'root',
-            'HOST': '0.0.0.0',
+    "default": {
+       "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
 # Internationalization
@@ -121,28 +127,26 @@ LOGGING = {
 }
 
 
-# Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-#ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-#import os
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = 'staticfiles'
-#STATIC_URL = '/static/'
-
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'SMSApp.SMSUser'
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
