5a6014de57e61d4ef47346cde758a905a4ff6e81
Dan T <tomdanizer@gmail.com>
2014-05-09 03:08:30 +0000

    heroku

--- SMSReminder/settings.py
+++ SMSReminder/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.webdesign',
     'django.contrib.messages',
     'debug_toolbar',
+    'gunicorn',
     'localflavor',
     'django.forms',
     'widget_tweaks',
@@ -73,12 +74,12 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -113,7 +114,25 @@ LOGGING = {
 }
 
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
