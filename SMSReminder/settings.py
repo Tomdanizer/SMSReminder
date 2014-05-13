936a52e3b3a3629b02ccc6c9b733ed61f1656c38
Dan T <tomdanizer@gmail.com>
2014-05-13 01:36:41 +0000

    heroku

--- SMSReminder/settings.py
+++ SMSReminder/settings.py
@@ -143,10 +143,11 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) #os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
+#STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static'),
+   os.path.join(BASE_DIR, 'static/'),
 )
