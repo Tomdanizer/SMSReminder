f18ec98ee05689744188500577f5491e4465e32f
Dan T <tomdanizer@gmail.com>
2014-05-13 01:37:40 +0000

    heroku

--- SMSReminder/settings.py
+++ SMSReminder/settings.py
@@ -148,6 +148,4 @@ STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
 #STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static/'),
-)
+STATICFILES_DIRS = ()
