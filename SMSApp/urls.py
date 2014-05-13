fe5cfd3a07ee446c048b36df39aff5e404f9394b
Dan T <tomdanizer@gmail.com>
2014-05-13 01:13:48 +0000

    heroku deploy

--- SMSApp/urls.py
+++ SMSApp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from SMSApp import views
+from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = patterns('',
     # ex: /SMSApp/
