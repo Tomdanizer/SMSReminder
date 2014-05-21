0fc7ce065bffcccd70de12990f5841c667be208d
Dan T <tomdanizer@gmail.com>
2014-05-21 01:17:03 +0000

    heroku push

--- SMSReminder/settings.py
+++ SMSReminder/settings.py
@@ -80,20 +80,20 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smsreminder',
-        'USER': 'root',
-            'HOST': '0.0.0.0',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'smsreminder',
+#         'USER': 'root',
+#             'HOST': '0.0.0.0',
+#     }
+# }
 
-#DATABASES = {
-#    "default": {
-#       "ENGINE": "django.db.backends.postgresql_psycopg2",
-#    }
-#}
+DATABASES = {
+   "default": {
+      "ENGINE": "django.db.backends.postgresql_psycopg2",
+   }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
