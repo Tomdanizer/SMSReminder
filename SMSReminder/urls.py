21626f80dd9b0590d94221d02b0b72eeaa482516
Dan T <tomdanizer@gmail.com>
2014-05-21 00:20:57 +0000

    heroku push

--- SMSReminder/urls.py
+++ SMSReminder/urls.py
@@ -12,9 +12,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^SMSApp/', include('SMSApp.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^__debug__/', include(debug_toolbar.urls)),
+
 )
 if not settings.DEBUG:
     urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        (r'^__debug__/', include(debug_toolbar.urls)),
     )
